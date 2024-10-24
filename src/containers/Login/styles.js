0421cb13c79ddea526311826ebed83f71240b4a4
andremessa <messa.andre@gmail.com>
2024-10-24 00:06:36 -0300

    inicio tela de login

---
+++ src/containers/Login/styles.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import BackgroundLogin from '../../assets/background-login.svg';
+import Background from '../../assets/background.svg';
+
+export const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+`;
+
+export const LeftContainer = styled.div`
+  background: url('${BackgroundLogin}');
+  background-size: cover;
+  background-position: center;
+  
+  height: 100%;
+  width: 100%;
+  max-width: 50%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 80%;
+  }
+`;
+
+export const RightContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  height: 100%;
+  width: 100%;
+  max-width: 50%;
+
+  background: url('${Background}');
+  background-color: #363636;
+
+  p {
+    color: #fff;
+    font-size: 1.125rem;
+    font-weight: 800;
+
+    a {
+      text-decoration: underline;
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Title = styled.h2`
+  font-family: "Road Rage", sans-serif;
+  font-size: 2.5rem;
+  color: #fff;
+
+  span {
+    color: #9758A6;
+    ;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  padding: 1.25rem;
+  width: 100%;
+  max-width: 25rem;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3125rem;
+  width: 100%;
+
+  input {
+    width: 100%;
+    border: none;
+    height: 3.25rem;
+    border-radius: 0.3125rem;
+    padding: 0 1rem;
+  }
+
+  label {
+    font-size: 1.125rem;
+    font-weight: 600;
+    color: #fff;
+  }
+`;
+
+export const Button = styled.button`
+
+`;
